13686481c4db1e6388e4e6a1e4fc28020871b74b
Adhidarma Hadiwinoto <me@adhisimon.org>
2025-07-25 14:57:53 +0700

    Versioning

--- amqp-exchange-follow-tail.py
+++ amqp-exchange-follow-tail.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+VERSION="v0.0.1"
+
 parser = argparse.ArgumentParser(
     description="AMPQ exchange consumer",
     epilog="""
@@ -28,5 +30,8 @@ parser.add_argument(
     required=True,
     help="AMQP exchange name, can be set by AMQP_EXCHANGE environment."
 )
+
+parser.add_argument('--version', action='version', version=f"%(prog)s {VERSION}")
+
 
 args = parser.parse_args()
